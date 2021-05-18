fa49efce05a200bcd89647513e9ff38b4fae8c39
zwf <zwf19980423@gmail.com>
2021-05-18 20:29:25 +0800

    added authready

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,36 +3,40 @@ import Image from 'next/image';
 import { useContext } from 'react';
 import AuthContext from '../stores/authContext';
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   console.log(user);
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/guides">
-              <a>Guides</a>
-            </Link>
-          </li>
-          {!user && (
-            <li onClick={login} className="btn">
-              Login/Signup
-            </li>
+        <>
+          {authReady && (
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/guides">
+                  <a>Guides</a>
+                </Link>
+              </li>
+              {!user && (
+                <li onClick={login} className="btn">
+                  Login/Signup
+                </li>
+              )}
+              {user && <li>{user.email}</li>}
+              {user && (
+                <li onClick={logout} className="btn">
+                  logout
+                </li>
+              )}
+            </ul>
           )}
-          {user && <li>{user.email}</li>}
-          {user && (
-            <li onClick={logout} className="btn">
-              logout
-            </li>
-          )}
-        </ul>
+        </>
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
